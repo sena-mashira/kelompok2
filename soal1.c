5f7e2337f7d8910fd89ce9a647e303141f59fca8
senamashira <anandhkrsna@gmail.com>
2025-09-27 11:17:45 +0800

    bebas

---
+++ soal1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float angka1 = 6.0;
+    int angka2 = 4;
+
+    if (angka2) {
+        printf("Masuk");
+    } else {
+        printf("Tidak");
+    }
+
+    return 0;
+}
